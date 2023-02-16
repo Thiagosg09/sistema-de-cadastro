d60ad7fdffe8589b694acf798b2999a006442584
Thiagosg09 <Thiagosg09>
2023-02-16 09:48:47 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-consolo.log("Rodando o sistema de gerencimento de jogos")
+console.log("Rodando o sistema de gerencimento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+console.log("Enviando para o GitHub")
+
